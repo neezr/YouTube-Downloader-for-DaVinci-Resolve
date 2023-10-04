25f68a42d291fe587c84578317228ee66ebff9c9
Nizar <145998491+neezr@users.noreply.github.com>
2023-10-04 12:41:51 +0200

    🎬 initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# ~ YouTube Downloader ~
+Download and import YouTube videos to DaVinci Resolve immediately.
+You can keep editing while your download is running!
+
+
+
+
+## Usage:
+- Run this script from DaVinci Resolve's dropdown menu (Workspace > Scripts)
+- Select your project folder and paste a YouTube URL into the text field
+- The video will automatically be downloaded as .mp4-file with the highest available resolution, placed in your project folder and imported to your Media Pool
+
+## Install:
+- Copy the .py-file into the folder "%appdata%\Blackmagic Design\DaVinci Resolve\Support\Fusion\Scripts\Utility"
+- Install Python 3.7+
+- Install the python modules 'pytube' and 'emoji'
+	- --> open 'cmd' on Windows and execute 'pip install pytube emoji' in the command line
+	- --> or install via requirements.txt with 'pip install -r requirements.txt'
---
+++ YouTube Downloader.py
@@ -0,0 +1,105 @@
+#~ YouTube Downloader ~
+#created by nizar / version 1.0
+#contact: http://twitter.com/nizarneezR
+
+#Usage:
+#Run this script from DaVinci Resolve's dropdown menu (Workspace > Scripts)
+#Select your project folder and paste a YouTube URL into the text field
+#The video will automatically be downloaded as .mp4-file with the highest available resolution, placed in your project folder and imported to your Media Pool
+
+#Install:
+#Copy this .py-file into the folder "%appdata%\Blackmagic Design\DaVinci Resolve\Support\Fusion\Scripts\Utility"
+#Install the python modules 'pytube' and 'emoji'
+#	open cmd and execute 'pip install pytube emoji' in the command line
+#	or: install via requirements.txt with 'pip install -r requirements.txt'
+
+import os, re, sys, emoji, tkinter
+from pytube import YouTube
+from tkinter import *
+from tkinter import filedialog
+
+STANDARD_FILE_LOCATION = os.path.expandvars(r"%APPDATA%\Blackmagic Design\DaVinci Resolve\Support\Fusion\Scripts\Utility\YouTube Downloader")
+filelocation = STANDARD_FILE_LOCATION
+
+
+def downloadVideo(link):
+	if link == "":
+		pass
+	else:
+		yt = YouTube(link).streams.get_highest_resolution()
+		filename = emoji.replace_emoji(yt.default_filename, replace='').strip()
+
+		while " ." in filename: #remove trailing white spaces if last characters in title were emojis
+			filename = filename.replace(" .", ".")
+
+		print(f"Downloading {filename}...")
+
+
+		try:
+			yt.download(filelocation,filename=filename)
+		except:
+			print(f"Error: Could not download video at {link}")
+
+
+		print(f"Done! Downloaded {filename} to {filelocation}")
+		os.startfile(filelocation, operation="explore")
+
+
+		resolve.GetMediaStorage().AddItemsToMediaPool(os.path.join(filelocation, filename))
+
+
+#GUI
+
+def gui_download_event():
+	global l_entryField, l_downloadbutton
+	url = l_entryField.get()
+	if len(url) > 0:
+		l_downloadbutton.configure(state=DISABLED, text="Downloading...")
+		l_entryField.configure(state=DISABLED)
+		try:
+			downloadVideo(url)
+		except Exception: #RegexMatchError in pytube
+			pass
+		l_downloadbutton.configure(state=NORMAL, text="Download")
+		l_entryField.configure(state=NORMAL)
+		l_entryField.delete(0, END)
+
+
+def gui_change_filelocation_event():
+	global l_filelocation_label, filelocation
+	previous_filelocation = filelocation #resetting after canceling out the prompt window
+	filelocation = filedialog.askdirectory()
+	if not filelocation:
+		filelocation = previous_filelocation
+	l_filelocation_label.configure(text=filelocation)
+
+def gui_reset_filelocation_event():
+	global l_filelocation_label, filelocation, STANDARD_FILE_LOCATION
+	filelocation = STANDARD_FILE_LOCATION
+	l_filelocation_label.configure(text=filelocation)
+
+
+root = Tk()
+root.title("Nizar's YouTube Downloader for DaVinci Resolve")
+BG_COLOR = '#28282e'
+FG_COLOR = '#cac5c4'
+root.configure(background=BG_COLOR)
+
+
+l_text = Label(root, text="Enter YouTube URL:", bg=BG_COLOR, fg=FG_COLOR)
+l_text.grid(row=0, column=0, sticky="W", padx=20, pady=10)
+l_entryField = Entry(root, width=50)
+l_entryField.grid(row=1,column=0, sticky="W", padx=20)
+l_downloadbutton = Button(root, text="Download", padx=50, command=gui_download_event)
+l_downloadbutton.grid(row=2, column=0, sticky="W", padx=20, pady=10)
+
+
+l_filelocation_label = Label(root, text=filelocation, anchor="w", bg=BG_COLOR, highlightbackground = "gray", highlightthickness=2, fg=FG_COLOR)
+l_filelocation_label.grid(row=4,column=0, sticky="W", padx=20, pady=(60,10))
+l_filelocation_button = Button(root, text="Change download folder", padx=50, command=gui_change_filelocation_event)
+l_filelocation_button.grid(row=5,column=0, sticky="W", padx=20)
+l_filereset_button = Button(root, text="Reset download folder", padx=50, command=gui_reset_filelocation_event)
+l_filereset_button.grid(row=5, column=1, sticky="W", padx=20)
+
+
+root.mainloop()
